e4128fdb0faa25fce4ca8d38511b69f79a4ba3f8
Andrey Stroganov <savthe@gmail.com>
2025-05-18 12:43:55 +0300

    Solutions

---
+++ cf_974_div3/a.rs
@@ -0,0 +1,96 @@
+#![allow(unused_imports, dead_code, non_camel_case_types)]
+
+fn solve() {
+    let (_, k): (us, us) = read2();
+    let a: Vec<us> = read_vec();
+    let mut s = 0;
+    let mut res = 0;
+    for &x in a.iter() {
+        if x >= k {
+            s += x;
+        }
+        if x == 0 && s > 0 {
+            res += 1;
+            s -= 1;
+        }
+    }
+    println!("{}", res);
+}
+
+fn main() {
+    for _ in 0..inp::read1() {
+        solve();
+    }
+}
+
+/* Auxiliary stuff */
+type us = usize;
+
+use std::collections::BTreeMap;
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+use inp::{read1, read2, read3, read_vec};
+
+mod inp {
+    use std::io;
+    use std::str::FromStr;
+
+    pub fn read_matrix<T: FromStr>(n: usize) -> Vec<Vec<T>> {
+        let mut a: Vec<Vec<T>> = vec![];
+        for _ in 0..n {
+            a.push(read_vec::<T>());
+        }
+        a
+    }
+
+    pub fn read_vec<T: FromStr>() -> Vec<T> {
+        read_line()
+            .split_whitespace()
+            .map(|x| x.parse().ok().unwrap())
+            .collect::<Vec<T>>()
+    }
+
+    pub fn read_line() -> String {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        input.trim_end().to_string()
+    }
+
+    pub fn read_bytes() -> Vec<u8> {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        input.trim_end().bytes().collect()
+    }
+
+    pub fn read1<T: FromStr>() -> T {
+        read_line().trim().parse().ok().unwrap()
+    }
+
+    pub fn read2<T1: FromStr, T2: FromStr>() -> (T1, T2) {
+        let input = read_line();
+        let mut i = input.split_whitespace();
+        let a: T1 = i.next().unwrap().parse().ok().unwrap();
+        let b: T2 = i.next().unwrap().parse().ok().unwrap();
+        (a, b)
+    }
+
+    pub fn read3<T1: FromStr, T2: FromStr, T3: FromStr>() -> (T1, T2, T3) {
+        let input = read_line();
+        let mut i = input.split_whitespace();
+        let a: T1 = i.next().unwrap().parse().ok().unwrap();
+        let b: T2 = i.next().unwrap().parse().ok().unwrap();
+        let c: T3 = i.next().unwrap().parse().ok().unwrap();
+        (a, b, c)
+    }
+
+    pub fn read4<T1: FromStr, T2: FromStr, T3: FromStr, T4: FromStr>() -> (T1, T2, T3, T4) {
+        let input = read_line();
+        let mut i = input.split_whitespace();
+        let a: T1 = i.next().unwrap().parse().ok().unwrap();
+        let b: T2 = i.next().unwrap().parse().ok().unwrap();
+        let c: T3 = i.next().unwrap().parse().ok().unwrap();
+        let d: T4 = i.next().unwrap().parse().ok().unwrap();
+        (a, b, c, d)
+    }
+}
